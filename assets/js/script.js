47cefccdb705a27ecb81dd0a3802d1a101a5fa9e
ReenaH <Reena_Hunjan@yahoo.com>
2022-05-28 20:37:26 -0400

    initial setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-console.log('opp');
+var QuestionArry = [
+    {question: "Question1", answerOptions: ["answer1","answer111", "answer1111", "answer11111"]},
+    {question: "Question2", answerOptions: ["answer12","answer112", "answer1112", "answer11112"]},
+    {question: "Question3", answerOptions: ["answer13","answer113", "answer1113", "answer11113"]},
+    {question: "Question4", answerOptions: ["answer14","answer114", "answer1114", "answer11114"]},
+     {question:"Question5", answerOptions: ["answer15","answer115", "answer1114", "answer11115"]},
+
+]; // end of QuestionArray
