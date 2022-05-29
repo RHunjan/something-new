e42b3c064a099a0fa1e392abedefb5efb2bac0e4
ReenaH <Reena_Hunjan@yahoo.com>
2022-05-28 21:32:44 -0400

    setting up initial files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var QuestionArry = [
+var questionArray = [
     {question: "Question1", answerOptions: ["answer1","answer111", "answer1111", "answer11111"]},
     {question: "Question2", answerOptions: ["answer12","answer112", "answer1112", "answer11112"]},
     {question: "Question3", answerOptions: ["answer13","answer113", "answer1113", "answer11113"]},
@@ -6,3 +6,21 @@ var QuestionArry = [
      {question:"Question5", answerOptions: ["answer15","answer115", "answer1114", "answer11115"]},
 
 ]; // end of QuestionArray
+
+// identify DOM elements
+var questionToAsk = document.querySelector(".questionAsked");
+var listOfAnswers = document.querySelector(".potential-answers");
+ // turn each question into an object
+
+ var eachQuestion = function(array){
+
+    //display question
+
+    questionToAsk.textContent = questionArray[0].question;
+    var answerArray = questionArray[0].answerOptions;
+    console.log(answerArray);
+  ;
+    
+ }; // end of function eachQuestion
+
+ eachQuestion(questionArray);
