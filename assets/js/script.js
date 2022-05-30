ef8edfa7bf253ed8c648583b4dbb6b77b970ffae
ReenaH <Reena_Hunjan@yahoo.com>
2022-05-29 22:29:43 -0400

    reduced complexity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 var questionArray = [
-    {question: "Question1", a: "Q1A1", b:"Q1A2", c: "Q1A3", d: "Q1A4", correctAnswer: 'b'},
+    {question: "Question1", a: "Q1A1", b:"Q1A2", c: "Q1A3", d: "Q1A4", correctAnswer: 'a'},
     {question: "Question2", a: "Q2A1", b:"Q2A2", c: "Q2A3", d: "Q2A4", correctAnswer: 'b'},
     {question: "Question3", a: "Q3A1", b:"Q3A2", c: "Q3A3", d: "Q3A4", correctAnswer: 'c'},
-    {question: "Question4", a: "Q4A1", b:"Q4A2", c: "Q4A3", d: "Q4A4", correctAnswer: 'a'},
-    {question: "Question5", a: "Q5A1", b:"Q5A2", c: "Q5A3", d: "Q5A4", correctAnswer: 'b'},
+    {question: "Question4", a: "Q4A1", b:"Q4A2", c: "Q4A3", d: "Q4A4", correctAnswer: 'd'},
+    {question: "Question5", a: "Q5A1", b:"Q5A2", c: "Q5A3", d: "Q5A4", correctAnswer: 'a'},
 ]; // end of QuestionArray
 
 //Hide instructions
@@ -14,17 +14,7 @@ var startButtonEl = document.getElementById("start-button");
 
 var questionContainerEl = document.querySelector(".question-container");
 
-startButtonEl.addEventListener("click", function(){
-  currentQuestion = 0;
-  // hide instructions
- instructionsEl.style.display = "none";
- // show quiz
- questionContainerEl.style.display = "block";
- // start timer
- startTimer();
- // show first question
-  showQuestion();
-});
+
 
 //global time variables
   let timeSeconds = 0;
@@ -51,112 +41,101 @@ var startTimer = function(){
   }; // end of start timer
 
   
-  let currentQuestion = 0;
-  let score = 0;
+
   
-
-  //Displaying the question
-  var showQuestion = function (){
-
     // select DOM elements
     var messageEl = document.getElementById("message");
-    var questionDisplayed = document.getElementById("questionAsked");
+    var questionDisplayedEl = document.getElementById("questionAsked");
     var answerAEl = document.getElementById("a");
     var answerBEl = document.getElementById("b");
     var answerCEl = document.getElementById("c");
     var answerDEl = document.getElementById("d");
- 
-  
-    questionDisplayed.textContent = questionArray[currentQuestion].question
+
+    let currentQuestion = 0;
+    let score = 0;
+
+      //Displaying the first question
+  var showQuestion = function (){
+    questionDisplayedEl.textContent = questionArray[currentQuestion].question
     answerAEl.textContent = questionArray[currentQuestion].a;
     answerBEl.textContent = questionArray[currentQuestion].b;
     answerCEl.textContent = questionArray[currentQuestion].c;
     answerDEl.textContent = questionArray[currentQuestion].d;
 
-    // check answers
-
-    // check button a
+    // add click events to a,b,c,d buttons
+    // button a
     answerAEl.addEventListener("click", function(){
-      if (answerAEl.id === questionArray[currentQuestion].correctAnswer){
-        messageEl.textContent = "RIGHT!";
-        currentQuestion++;
-        console.log(currentQuestion);
-        score = score + 10;
-        console.log(score);
-        showQuestion();
-         } else {
-        messageEl.textContent = "WRONG!";
-        currentQuestion++;
-         console.log(currentQuestion);
-        timeSeconds = timeSeconds-15;
-        showQuestion();
-         }
-     
-    }); // end onclick button A
-         
+      console.log('purple');
+      checkAnswers(answerAEl);
+    });
   
-     // check button b
-    answerBEl.addEventListener("click", function(){
-      if (answerBEl.id === questionArray[currentQuestion].correctAnswer){
+
+      //button b
+     answerBEl.addEventListener("click", function(){
+      console.log('blue');
+      checkAnswers(answerBEl);
+    });
+
+    //button c
+     answerCEl.addEventListener("click", function(){
+      console.log('green');
+      checkAnswers(answerCEl);
+    });
+
+    // button d
+     answerDEl.addEventListener("click", function(){
+      console.log('yellow');
+     checkAnswers(answerDEl);
+    });
+
+    //check answers
+
+    var checkAnswers = function(x){
+      if (x.id === questionArray[currentQuestion].correctAnswer){
         messageEl.textContent = "RIGHT!";
-        currentQuestion++;
-        console.log(currentQuestion);
-        score = score + 10;
-        console.log(score);
-        showQuestion();
-         } else {
+      } else {
         messageEl.textContent = "WRONG!";
-        currentQuestion++;
-         console.log(currentQuestion);
-        timeSeconds = timeSeconds-15;
-        showQuestion();
-         }
-     
-    }); // end onclick button b
-         
-      // check button c
-    answerCEl.addEventListener("click", function(){
-      if (answerCEl.id === questionArray[currentQuestion].correctAnswer){
-        messageEl.textContent = "RIGHT!";
-        currentQuestion++;
-        console.log(currentQuestion);
-        score = score + 10;
-        console.log(score);
-        showQuestion();
-         } else {
-        messageEl.textContent = "WRONG!";
-        currentQuestion++;
-         console.log(currentQuestion);
-        timeSeconds = timeSeconds-15;
-        showQuestion();
-         }
-     
-    }); // end onclick button c
+      }
+      currentQuestion++;
+      console.log(currentQuestion);
+   
 
-       // check button d
-    answerDEl.addEventListener("click", function(){
-      if (answerDEl.id === questionArray[currentQuestion].correctAnswer){
-        messageEl.textContent = "RIGHT!";
-        currentQuestion++;
-        console.log(currentQuestion);
-        score = score + 10;
-        console.log(score);
-        showQuestion();
-         } else {
-        messageEl.textContent = "WRONG!";
-        currentQuestion++;
-         console.log(currentQuestion);
-        timeSeconds = timeSeconds-15;
-        showQuestion();
-         }
-     
-    }); // end onclick button d
+    };
 
 
-     }; // end of showTheFirstQuestion
- 
- 
 
+  }; // end of first question
+
+  //move to next question
+
+  var nextQuestion = function(){
+    showQuestion();
+  };
+
+
+  //var nextQuestion = function(){};
+
+
+
+
+
+
+
+
+// Start the Quiz
+ startButtonEl.addEventListener("click", function(){
+
+  // hide instructions
+ instructionsEl.style.display = "none";
+ // show quiz
+ questionContainerEl.style.display = "block";
+ // start timer
+ startTimer();
+ // show first question
+  showQuestion();
+});
+
+  
 
 
 
